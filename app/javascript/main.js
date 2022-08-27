8e959b0cbc87795cc5328603c0fd365037cdd7b3
AbdullahButt2611 <abutt2210@gmail.com>
2022-08-27 19:50:50 +0500

    Sidebar codecommented

--- app/javascript/main.js
+++ app/javascript/main.js
@@ -16,19 +16,19 @@ document.addEventListener("turbolinks:load", function() {
 
     // ==============================       SideBar     ==========================================
 
-    let btn = document.querySelector("#btn");
-    let sidebar = document.querySelector(".sidebar");
-    let searchBtn = document.querySelector(".bx-search");
-    btn.style.cursor = "pointer";
-    searchBtn.style.cursor = "pointer";
+    // let btn = document.querySelector("#btn");
+    // let sidebar = document.querySelector(".sidebar");
+    // let searchBtn = document.querySelector(".bx-search");
+    // btn.style.cursor = "pointer";
+    // searchBtn.style.cursor = "pointer";
 
-    btn.onclick = function(){
-        sidebar.classList.toggle("active");
-    }
+    // btn.onclick = function(){
+    //     sidebar.classList.toggle("active");
+    // }
 
-    searchBtn.onclick = function(){
-        sidebar.classList.toggle("active");
-    }
+    // searchBtn.onclick = function(){
+    //     sidebar.classList.toggle("active");
+    // }
 })
 
 
