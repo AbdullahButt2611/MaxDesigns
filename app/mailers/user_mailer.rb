cbd40098bf5729e5d093b4d581a95441d6c7afdd
AbdullahButt2611 <abutt2210@gmail.com>
2022-09-02 14:55:28 +0500

    Send Grid Implanted into the Project

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -6,7 +6,7 @@ class UserMailer < ApplicationMailer
 		@user = params[:user]
 		@url = 'hhtp://example.com/login'
 
-		mail(to: @user.email, subject: "Welcome to My awesome Site")
+		mail(to: @user.email, subject: "Welcome to Max Designs")
 	end  
 
 end
--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,19 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+
+ActionMailer::Base.smtp_settings = {
+
+  # This is the string literal 'apikey', NOT the ID of your API key
+  :user_name => 'apikey', 
+
+  # This is the secret sendgrid API key which was issued during API key creation
+  :password => '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', 
+
+  :domain => 'max-designs.herokuapp.com',
+  :address => 'smtp.sendgrid.net',
+  :port => 587,
+  :authentication => :plain,
+  :enable_starttls_auto => true
+}
