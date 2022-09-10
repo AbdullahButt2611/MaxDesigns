d5b724db1102998ae61a2d1ed313438f6bf1dc81
AbdullahButt2611 <abutt2210@gmail.com>
2022-09-10 16:08:43 +0500

    Projects Backend Competed

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -2,6 +2,7 @@ class ProjectsController < ApplicationController
 	before_action :authenticate_user!
 
 	def index
+		@projects = Project.all
 	end
 
 	def new
@@ -19,7 +20,26 @@ class ProjectsController < ApplicationController
 	    else
 	      render :new
 	    end
+  	end
 
+  	def edit
+		@project = Project.find(params[:id])
+	end
+
+	def update
+		@project = Project.find(params[:id])
+
+		if @project.update(project_params)
+	      redirect_to projects_path
+	    else
+	      render :edit
+	    end
+	end
+
+	def destroy
+  		@project = Project.find(params[:id])
+  		@project.destroy
+  		redirect_to projects_path
   	end
 
 
