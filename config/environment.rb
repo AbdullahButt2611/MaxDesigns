f6256934da527136c415cbb08aedea68d599e3f6
AbdullahButt2611 <abutt2210@gmail.com>
2022-09-10 19:36:27 +0500

    Changed the mailer in devise file

--- config/environment.rb
+++ config/environment.rb
@@ -5,17 +5,3 @@ require_relative 'application'
 Rails.application.initialize!
 
 
-ActionMailer::Base.smtp_settings = {
-
-  # This is the string literal 'apikey', NOT the ID of your API key
-  user_name: 'apikey', 
-
-  # This is the secret sendgrid API key which was issued during API key creation
-  password: Rails.application.credentials.dig(:sendgrid, :api_key), 
-
-  domain: 'max-designs.herokuapp.com',
-  address: 'smtp.sendgrid.net',
-  port: 587,
-  authentication: :plain,
-  enable_starttls_auto: true
-}
