3150c99ee95db7eb334d864db625aef56ec8db76
AbdullahButt2611 <abutt2210@gmail.com>
2022-09-12 16:41:31 +0500

    Project Index

---
+++ app/helpers/projects_helper.rb
@@ -0,0 +1,9 @@
+module ProjectsHelper
+
+	def project_deadline_formatiing(my_date)
+		require 'date'
+		# str =  Date::MONTHNAMES[date_obj.month]  +" "+date_obj.day.to_s+", "+date_obj.year.to_s
+		return Date::MONTHNAMES[my_date.month]+" "+my_date.day.to_s+", "+my_date.year.to_s
+		return my_date
+	end
+end
