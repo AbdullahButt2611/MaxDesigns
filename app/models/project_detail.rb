d93b2187a8a0b93a892940d688bbe11577ea7630
AbdullahButt2611 <abutt2210@gmail.com>
2022-09-25 22:08:52 +0500

    User Pays Section added with user data

--- app/models/project_detail.rb
+++ app/models/project_detail.rb
@@ -3,5 +3,8 @@ class ProjectDetail < ApplicationRecord
     belongs_to :order, optional: true, touch: true
     belongs_to :user
 
+    scope :details_added_by_user, ->(id) {where(user_id: id)}
+    scope :details_added_by_user_in_month, ->(id, month) {where(user_id: id).where('extract(year from updated_at) = ?', DateTime.now.year.to_i).where('extract(month  from updated_at) = ?', month.to_i)}
+
     validates :task, presence: true, length: { minimum: 5 }
 end
