564e18c3b6999565cedf44406b847a443c0eda61
AbdullahButt2611 <abutt2210@gmail.com>
2022-09-27 01:15:15 +0500

    Changed file

--- config/initializers/wicked_pdf.rb
+++ config/initializers/wicked_pdf.rb
@@ -9,7 +9,7 @@
 # https://github.com/mileszs/wicked_pdf/blob/master/README.md
 
 WickedPdf.config = {
-  exe_path: '/usr/local/bin/wkhtmltopdf',
+  exe_path: Gem.bin_path('wkhtmltopdf-binary', 'wkhtmltopdf')
   # exe_path: '/usr/bin/wkhtmltopdf',
   enable_local_file_access: true,
   
